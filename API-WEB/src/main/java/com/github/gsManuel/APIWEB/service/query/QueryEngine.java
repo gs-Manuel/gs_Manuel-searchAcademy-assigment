37a9353aabda7f9c10bf4e3f747c98a47e75af30
Manuel <gs_manuel@outlook.com>
2022-11-16 13:24:02 +0100

    Removed unused files
    Removed basic query search

--- API-WEB/src/main/java/com/github/gsManuel/APIWEB/service/query/QueryEngine.java
+++ API-WEB/src/main/java/com/github/gsManuel/APIWEB/service/query/QueryEngine.java
@@ -1,28 +1,138 @@
 package com.github.gsManuel.APIWEB.service.query;
 
+import co.elastic.clients.elasticsearch._types.FieldValue;
+import co.elastic.clients.elasticsearch._types.query_dsl.*;
+import co.elastic.clients.json.JsonData;
 import com.github.gsManuel.APIWEB.model.QueryRespone;
 import com.github.gsManuel.APIWEB.service.elastic.ElasticService;
 import org.apache.tomcat.util.json.JSONParser;
 import org.apache.tomcat.util.json.ParseException;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class QueryEngine implements QueryService{
+    /**
+     * Creates a multimatch query
+     *
+     * @param query  - query to search
+     * @param fields - fields to search
+     * @return Query
+     */
+    @Override
+    public Query multiMatch(String query, String[] fields) {
+        Query multiMatchQuery = MultiMatchQuery.of(m -> m
+                .query(query)
+                .fields(Arrays.stream(fields).toList()))._toQuery();
 
-        private ElasticService elasticService;
-
-        public QueryEngine(ElasticService elasticService){
-            this.elasticService = elasticService;
-        }
-
-        @Override
-        public QueryRespone search(String query) {
-            String elasticInfo = elasticService.getElasticInfo();
-            //Parse the json above to obtain the cluster name
-            String clusterName = "";
-            try {
-                clusterName = new JSONParser(elasticInfo).parseObject().get("cluster_name").toString();
-            } catch (ParseException e) {
-                throw new RuntimeException(e);
-            }
-            return new QueryRespone(query, clusterName);
-        }
+        return multiMatchQuery;
     }
+
+    /**
+     * Creates a term query
+     *
+     * @param field Field to search
+     * @param value Value to search
+     * @return Query
+     */
+    @Override
+    public Query termQuery(String value, String field) {
+        Query termQuery = TermQuery.of(t -> t
+                .value(value)
+                .field(field))._toQuery();
+
+        return termQuery;
+    }
+
+    /**
+     * Creates a list of term queries
+     *
+     * @param values - values to search
+     * @param field  - field to search
+     * @return List of term queries
+     */
+    @Override
+    public List<Query> termQueries(String[] values, String field) {
+        List<Query> termQueries = Arrays.stream(values)
+                .map(value -> termQuery(value, field))
+                .collect(Collectors.toList());
+
+        return termQueries;
+    }
+
+    /**
+     * Creates a terms query
+     *
+     * @param values Values to search
+     * @param field  Field to search
+     * @return Query
+     */
+    @Override
+    public Query termsQuery(String[] values, String field) {
+        TermsQueryField termsQueryField = TermsQueryField.of(t -> t
+                .value(Arrays.stream(values).toList().stream().map(FieldValue::of).collect(Collectors.toList())));
+
+        Query termsQuery = TermsQuery.of(t -> t
+                .field(field)
+                .terms(termsQueryField))._toQuery();
+
+        return termsQuery;
+    }
+
+    /**
+     * Creates a bool query
+     *
+     * @param queries List of queries to be executed
+     * @return Query to be executed
+     */
+    @Override
+    public Query boolQuery(List<Query> queries) {
+        Query boolQuery = BoolQuery.of(b -> b.filter(queries))._toQuery();
+        return boolQuery;
+    }
+
+    /**
+     * Creates a should query
+     *
+     * @param queries List of queries to be executed
+     * @return Query to be executed
+     */
+    @Override
+    public Query shouldQuery(List<Query> queries) {
+        Query shouldQuery = BoolQuery.of(b -> b.should(queries))._toQuery();
+        return shouldQuery;
+    }
+
+    /**
+     * Creates a range double filter
+     *
+     * @param field Field to search
+     * @param min   Min value
+     * @param max   Max value
+     * @return Query to be executed
+     */
+    @Override
+    public Query rangeDoubleQuery(String field, Double min, Double max) {
+        return RangeQuery.of(r -> r
+                .field(field)
+                .gte(JsonData.of(min))
+                .lte(JsonData.of(max)))._toQuery();
+    }
+
+    /**
+     * Creates a range integer filter
+     *
+     * @param field Field to search
+     * @param min   Min value
+     * @param max   Max value
+     * @return Query to be executed
+     */
+    @Override
+    public Query rangeIntegerQuery(String field, Integer min, Integer max) {
+        return RangeQuery.of(r -> r
+                .field(field)
+                .gte(JsonData.of(min))
+                .lte(JsonData.of(max)))._toQuery();
+    }
+}
--- API-WEB/src/main/java/com/github/gsManuel/APIWEB/service/query/QueryService.java
+++ API-WEB/src/main/java/com/github/gsManuel/APIWEB/service/query/QueryService.java
@@ -1,7 +1,80 @@
 package com.github.gsManuel.APIWEB.service.query;
 
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import com.github.gsManuel.APIWEB.model.QueryRespone;
 
+import java.util.List;
+
 public interface QueryService {
-    QueryRespone search(String query);
+    /**
+     * Creates a multi match query
+     *
+     * @param query  - query to search
+     * @param fields - fields to search
+     * @return Query to be executed
+     */
+    Query multiMatch(String query, String[] fields);
+
+    /**
+     * Performs a term query
+     *
+     * @param field - field to search
+     * @param value - value to search
+     * @return Query to be executed
+     */
+    Query termQuery(String value, String field);
+
+    /**
+     * Creates a list of term queries
+     *
+     * @param values - values to search
+     * @param field  - field to search
+     * @return List of term queries
+     */
+    List<Query> termQueries(String[] values, String field);
+
+    /**
+     * Performs a terms query
+     *
+     * @param values Values to search
+     * @param field  Field to search
+     * @return Query to be executed
+     */
+    Query termsQuery(String[] values, String field);
+
+    /**
+     * Creates a bool query
+     *
+     * @param queries List of queries to be executed
+     * @return Query to be executed
+     */
+    Query boolQuery(List<Query> queries);
+
+    /**
+     * Creates a should query
+     *
+     * @param queries List of queries to be executed
+     * @return Query to be executed
+     */
+    Query shouldQuery(List<Query> queries);
+
+    /**
+     * Creates a range double query
+     *
+     * @param field Field to search
+     * @param min   Min value
+     * @param max   Max value
+     * @return Query to be executed
+     */
+    Query rangeDoubleQuery(String field, Double min, Double max);
+
+    /**
+     * Creates a range integer query
+     *
+     * @param field Field to search
+     * @param min   Min value
+     * @param max   Max value
+     * @return Query to be executed
+     */
+    Query rangeIntegerQuery(String field, Integer min, Integer max);
 }
